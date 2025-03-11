3b546a1286b2d96892be69c827205e34ce034345
Rafael Luis da Costa Coelho <rafaelluisdacostacoelho@gmail.com>
2025-03-11 19:39:50 -0300

    Add base project.

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FeaturesComponent } from "./components/features/features.component";
+import { TestimonialsComponent } from "./components/testimonials/testimonials.component";
+import { PricingComponent } from "./components/pricing/pricing.component";
+import { ContactFormComponent } from "./components/contact-form/contact-form.component";
+import { FooterComponent } from "./components/footer/footer.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    HeroSectionComponent,
+    NavbarComponent,
+    FeaturesComponent,
+    TestimonialsComponent,
+    PricingComponent,
+    ContactFormComponent,
+    FooterComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'hi-snapqr-leads';
+}
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
