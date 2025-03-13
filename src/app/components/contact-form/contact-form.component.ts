ebf05eed2285bc76085d79e98fb92f4ace17edc1
Rafael Luis da Costa Coelho <rafaelluisdacostacoelho@gmail.com>
2025-03-13 00:04:33 -0300

    Add fine adjusments in contact form.

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { ContactService } from '../../services/contact.service';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  contactForm: FormGroup;
+  submitted = signal(false);
+  loading = signal(false);
+  successMessage = signal('');
+  errorMessage = signal('');
 
+  constructor(private fb: FormBuilder, private contactService: ContactService) {
+    this.contactForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: [''],
+      company: [''],
+      jobTitle: [''],
+      interest: ['', Validators.required],
+      message: [''],
+      subscribe: [false]
+    });
+  }
+
+  get f() {
+    return this.contactForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted.set(true);
+    this.successMessage.set('');
+    this.errorMessage.set('');
+
+    if (this.contactForm.invalid) {
+      return;
+    }
+
+    this.loading.set(true);
+
+    this.contactService.sendContact(this.contactForm.value).subscribe({
+      next: () => {
+        this.successMessage.set('Contato enviado com sucesso!');
+        this.contactForm.reset();
+        this.submitted.set(false);
+      },
+      error: () => {
+        this.errorMessage.set('Erro ao enviar contato. Tente novamente.');
+      }
+    }).add(() => this.loading.set(false));
+  }
 }
