0725e9c084cdbad21351991c72caaf8a46e2c5d6
Rafael Luis da Costa Coelho <rafaelluisdacostacoelho@gmail.com>
2025-03-21 02:40:54 -0300

    Add success and failure pages to checkout for lifetime plan.

---
+++ src/app/components/checkout-success/checkout-success.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-checkout-success',
+  imports: [],
+  templateUrl: './checkout-success.component.html',
+  styleUrl: './checkout-success.component.css'
+})
+export class CheckoutSuccessComponent {
+  constructor(private router: Router) { }
+
+  goHome() {
+    this.router.navigate(['/']);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@ export const environment = {
   featureFlag: true,
   stripe: {
     lifetimePlan: {
-      productId: 'prod_RymIt4u0nXQ8gV',
-      priceId: 'price_1R4ojTK1X1BwQjLlqrfuHF7H',
-      couponId: 'dG6R9Tam',
+      productId: 'prod_RyvpHChdvmCfQv',
+      priceId: 'price_1R4xxYK1X1BwQjLlde6Ajnyw',
+      couponId: '2K3qUJyx',
     }
   }
 };
